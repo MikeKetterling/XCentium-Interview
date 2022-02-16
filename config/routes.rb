96d5f066b3daff08760a033f1a32ad3b20c11009
Mike <mikekett4@yahoo.com>
2022-02-15 17:03:33 -0700

    completed back end set up and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
+
+  get "/me", to: "users#show"
+  post "/login", to:"sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
