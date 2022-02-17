55ae2a9a8c7ead68c8131a69e386bee73ae89cb4
Mike Ketterling <91397183+MikeKetterling@users.noreply.github.com>
2022-02-16 23:26:12 -0700

    Update App.js

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,7 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useContext, useEffect, useState} from "react";
-import {NavLink, Route, Routes, useNavigate} from "react-router-dom"
+import {useContext, useEffect} from "react";
+import {NavLink, Route, Routes} from "react-router-dom"
 import Login from './Login';
 import Home from './Home';
 import { AuthContext } from '../Context/authentication';
